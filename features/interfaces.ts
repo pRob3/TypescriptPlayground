949cb9c41c52058f9908985d0330064215882053
pRob3 <bubblan@slaf.se>
2022-07-25 16:05:09 +0200

    Functions in interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,7 +1,4 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+interface Reportable {
   summary(): string;
 }
 
@@ -14,12 +11,8 @@ const oldCivic = {
   },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.summary());
-
-  console.log(
-    `Name: ${vehicle.name}, Year: ${vehicle.year}, Broken: ${vehicle.broken}`
-  );
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
