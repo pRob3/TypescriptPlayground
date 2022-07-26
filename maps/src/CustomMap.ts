8bdf3dc644846c22f2c45a85e01eb05a84e62362
pRob3 <bubblan@slaf.se>
2022-07-26 14:26:01 +0200

    Popup and interface

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,13 @@
+// Instructions to every other class
+// on how they can be an argument to addMarker
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -6,6 +16,21 @@ export class CustomMap {
     this.googleMap = new google.maps.Map(mapDiv!, {
       zoom: 1,
       center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: { lat: mappable.location.lat, lng: mappable.location.lng },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
