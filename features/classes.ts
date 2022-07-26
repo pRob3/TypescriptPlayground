09fcb2538c4c7d2c34ac57da91e19f1e6045172c
pRob3 <bubblan@slaf.se>
2022-07-26 08:30:20 +0200

    Fields in Classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,22 +1,25 @@
 class Vehicle {
+  constructor(public color: string) {}
+
   protected honk(): void {
     console.log('beep! beep!');
   }
 }
 
-const vehicle = new Vehicle();
+const vehicle = new Vehicle('pink');
 // vehicle.honk(); // error: honk is protected
+console.log(vehicle.color);
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log('vroom! vroom!');
-  }
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log('vroom! vroom!');
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const car = new Car();
-car.startDrivingProcess();
+// const car = new Car();
+// car.startDrivingProcess();
