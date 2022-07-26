4279f3f7abbeb4a1bf6e9660d594d486c4c854a5
pRob3 <bubblan@slaf.se>
2022-07-26 13:24:18 +0200

    Google map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,10 @@
-import { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-const company = new Company();
+const mapDiv = document.getElementById('map');
 
-console.log(user);
-console.log(company);
+new google.maps.Map(mapDiv!, {
+  zoom: 1,
+  center: { lat: 0, lng: 0 },
+});
